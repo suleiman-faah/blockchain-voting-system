3c10a9183c4ab3e33bea7eced814675a7b852891
Gyen Abubakar <indehyde@gmail.com>
2023-09-14 16:51:05 +0000

    implemented admin add-candidates page

--- app/admin/(admin-dashboard)/add-candidates/page.jsx
+++ app/admin/(admin-dashboard)/add-candidates/page.jsx
@@ -1,9 +1,66 @@
 'use client';
 
+import { Field, Form, FormLayout, SubmitButton } from '@saas-ui/react';
+
 const AddCandidatePage = () => {
+  function onSubmit(params) {
+    console.log('onSubmit:', params);
+  }
+
   return (
     <>
-      <h2>Add Candidate</h2>
+      <Form
+        className="max-w-[800px] mx-auto mt-12"
+        defaultValues={{
+          name: '',
+          age: 40,
+          party: '',
+          qualification: '',
+        }}
+        onSubmit={onSubmit}
+      >
+        <FormLayout>
+          <Field
+            name="name"
+            label="Name"
+            type="text"
+            isRequired
+            rules={{ required: true }}
+            placeholder="Enter candidate name"
+          />
+
+          <Field
+            name="age"
+            label="Age"
+            type="number"
+            placeholder="Enter candidate age"
+            isRequired
+            rules={{ required: true }}
+          />
+
+          <Field
+            name="party"
+            label="Party"
+            type="text"
+            placeholder="Enter candidate party"
+            isRequired
+            rules={{ required: true }}
+          />
+
+          <Field
+            name="qualification"
+            label="Qualification"
+            type="text"
+            placeholder="Enter candidate qualification"
+            isRequired
+            rules={{ required: true }}
+          />
+
+          <SubmitButton size="lg" className="mt-4 w-full bg-primary">
+            Add Candidate
+          </SubmitButton>
+        </FormLayout>
+      </Form>
     </>
   );
 };
