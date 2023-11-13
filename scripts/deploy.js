c93653707e3404ba8221c34f8b766c0b5aa7ad35
CharaD7 <jijakahn6@gmail.com>
2023-11-13 11:49:51 +0000

    Refactored code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -102,16 +102,16 @@ const createApp = async (sender,
         
         // Submit the transaction
         await client.sendRawTransaction(signedTxn).do()                           
-            // Wait for transaction to be confirmed
-           let confirmedTxn = await algosdk.waitForConfirmation(client, txId, 4);
-            console.log("confirmed" + confirmedTxn)
+        // Wait for transaction to be confirmed
+        let confirmedTxn = await algosdk.waitForConfirmation(client, txId, 4);
+        console.log("confirmed" + confirmedTxn)
 
-            //Get the completed Transaction
-            console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
-            // display results
-            let transactionResponse = await client.pendingTransactionInformation(txId).do()
-            let appId = transactionResponse['application-index'];
-            console.log("Created new app-id: ",appId);
+        //Get the completed Transaction
+        console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+        // display results
+        let transactionResponse = await client.pendingTransactionInformation(txId).do()
+        let appId = transactionResponse['application-index'];
+        console.log("Created new app-id: ",appId);
       }catch(err){
       console.log(err)
     }
